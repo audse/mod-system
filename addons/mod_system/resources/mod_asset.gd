e27889b462624e0ac423b4d04cff9f054e89036e
Audrey Serene <audrey.serene@gmail.com>
2023-06-28 22:00:10 -0500

    Write doc comments for ModAsset class (resolves #8)

--- addons/mod_system/resources/mod_asset.gd
+++ addons/mod_system/resources/mod_asset.gd
@@ -1,9 +1,21 @@
 class_name ModAsset extends Resource
 
+## Contains a [String] identifier and a [Resource], loaded once per associated [Mod]
+##
+## A [ModAsset] is a named [Resource] that is loaded when a [Mod] is registered. That way, all
+## instances of the mod share the same resource.
+## [br][br]This is also helpful so that you can get assets by referencing the [member key] rather
+## than worrying about loading/file paths.
+
+
+## An identifier for this asset, should be unique to the containing [Mod].
 @export var key: String
+
+## Any resource file.
 @export var asset: Resource
 
 
+## Constructs a new [ModAsset] with a given key and asset.
 static func create(key_value: String, asset_value: Resource) -> ModAsset:
 	var asset := ModAsset.new()
 	asset.key = key_value
@@ -11,12 +23,17 @@ static func create(key_value: String, asset_value: Resource) -> ModAsset:
 	return asset
 
 
+## Returns a [Resource] at the given [code]path[/code], if it exists. Otherwise, returns [code]null[/code].
 static func load_resource(path: String) -> Resource:
 	if ResourceLoader.exists(path):
 		return ResourceLoader.load(path)
 	return null
 
 
+## Returns a [Texture2D] containing the image at the given [code]path[/code], if it exists. 
+## Otherwise, returns [code]null[/code].
+## [br][br]This function also saves a new [code].import.tres[/code] file so that loading can be
+## faster next time.
 static func load_image(path: String) -> Texture2D:
 	# Try to load in project files
 	if path.begins_with("res://"):
