e4230365ec6799602efa1aed0255876305746983
Audrey Serene <audrey.serene@gmail.com>
2023-06-28 22:18:12 -0500

    Write doc comments for ModInstance and ModInstanceScript classes (resolves #6, #7)

--- addons/mod_system/resources/mod_instance.gd
+++ addons/mod_system/resources/mod_instance.gd
@@ -3,11 +3,16 @@ class_name ModInstance extends Object
 ## [ModInstance]s are created when a [Mod] is granted to an [Object].
 ##
 ## [Mod]s can only be granted when they are enabled.
-## [br] See [member ModSystem.enabled_mods] for more information on enabling mods
-## [br] See [method grant] for more information on granting mods
+## [br]See [member ModSystem.settings.enabled_mods] for more information on enabling mods
+## [br]See [method grant] for more information on granting mods
 
+## The [Mod] that has been granted
 var mod: Mod
+
+## The [Object] to which the [member mod] has been granted
 var owner: Object
+
+## The [ModInstanceScript] that should be run upon granting
 var instance_script: ModInstanceScript
 
 
@@ -20,6 +25,7 @@ func _init(mod_value: Mod, owner_value: Object) -> void:
 		instance_script = mod.instance_script.inner_script.new(self)
 
 
+## Grants the [member mod] to the [member owner], if it's enabled (see [member Mod.is_enabled]
 func grant() -> void:
 	if mod.is_enabled:
 		mod.instances[owner] = self
@@ -27,7 +33,7 @@ func grant() -> void:
 		ModSystem.mod_granted.emit(self)
 	else: revoke()
 
-
+## Revokes the [member mod] from the [member owner]
 func revoke() -> void:
 	if mod.is_enabled: 
 		mod.instances.erase(owner)
--- addons/mod_system/resources/mod_instance_script.gd
+++ addons/mod_system/resources/mod_instance_script.gd
@@ -1,5 +1,11 @@
 class_name ModInstanceScript extends Resource
 
+## The main script for handling the effects of your mods
+##
+## This is the class that should be extended when writing your mods. This is the script that will
+## be called in-game and be able to effect gameplay. See [member Mod.instance_script].
+
+## The [ModInstance] of which this script belongs
 var instance: ModInstance = null
 
 
@@ -28,53 +34,92 @@ func _init(instance_value: ModInstance) -> void:
 			instance.owner.connect(signal_name, owner_connections[signal_name])
 
 
+## If logging is enabled (see [member ModSettings.enable_logging]), prints the [code]message[/code]
+## to the terminal with contextual information about the current [Mod].
+## (in [member instance], see [member ModInstance.mod])
 func logs(message: String) -> void:
 	ModSystemUtils.mod_log(instance.mod, message)
 
 
+## Virtual method. Called when the mod is granted to the owner.
+## [br]•  The granted mod is [member ModInstance.mod] in [member instance]
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_granted() -> void:
 	pass
 
 
+## Virtual method. Called when the mod is revoked from the owner.
+## [br]•  The revoked mod is [member ModInstance.mod] in [member instance]
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_revoked() -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.child_entered_tree] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_child_entered_tree(_node: Node) -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.child_exiting_tree] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_child_exiting_tree(_node: Node) -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.child_order_changed] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_child_order_changed() -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Control.gui_input] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Control].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_gui_input(_event: InputEvent) -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.tree_entered] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_tree_entered() -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.ready] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_ready() -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.renamed] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_renamed() -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.replacing_by] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_replacing_by(_node: Node) -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.tree_exiting] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_tree_exiting() -> void:
 	pass
 
 
+## Virtual method. Called when the [signal Node.tree_exited] signal is emitted on the owner.
+## [br]•  Only applicable if the owner extends [Node].
+## [br]•  The owner is [member ModInstance.owner] in [member instance]
 func _on_owner_tree_exited() -> void:
 	pass
