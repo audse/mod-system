76afba09fd9ad8ed728fdc3eb4794b3d7cd1a610
Audrey Serene <audrey.serene@gmail.com>
2023-06-30 10:45:12 -0500

    Write doc comments for RegisteredClass class (resolves #9)

--- addons/mod_system/resources/registered_class.gd
+++ addons/mod_system/resources/registered_class.gd
@@ -1,20 +1,28 @@
 class_name RegisteredClass extends Object
 
+## Defines a [Script] associated with a unique class name
+##
+## A [RegisteredClass] defines a [Script] class with a given name. If the name can be either
+## the [member Script.resource_path] or provided explicitly.
+
+
+## The unique given name for this class – either [member Script.resource_path] or explicitly provided.
 var name: StringName
+## The class that is registered.
 var cls: Script
-var parent: RegisteredClass
+## The [RegisteredClass] that [member cls] [code]extends[/code].
+var parent: RegisteredClass = null
 
 
+## Constructs a new [RegisteredClass] with the given [member name], [member cls], and [member parent]
 func _init(name_value: StringName, cls_value: Script, parent_value: RegisteredClass = null) -> void:
 	name = name_value
 	cls = cls_value
 	parent = parent_value
 
 
-func instantiate() -> Object:
-	return cls.new()
-
-
+## Returns [code]true[/code] if the [member parent] (or the parent's [member parent], etc.)
+## is the [code]ancestor[/code].
 func has_ancestor(ancestor: RegisteredClass) -> bool:
 	return (
 		ancestor == parent
