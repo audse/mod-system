60818a22f2d42e3b664f0e4a580de98ddbc8f150
Audrey Serene <audrey.serene@gmail.com>
2023-07-07 23:00:17 -0500

    Add doc comments to ModScriptParser

--- addons/mod_system/utils/script_parser.gd
+++ addons/mod_system/utils/script_parser.gd
@@ -1,7 +1,8 @@
 class_name ModScriptParser extends Object
 
+## Utilities to help parse GDScript source code
 
-
+## Parses and returns the [code]extends[/code] class name from the provided [code]script[/code]'s source code
 static func get_extended_class(script: GDScript) -> StringName:
 	if script:
 		var regex := RegEx.create_from_string("extends[\\s\\t\\n]*([^\\s\\t\\n]+)")
@@ -10,6 +11,7 @@ static func get_extended_class(script: GDScript) -> StringName:
 	return &""
 
 
+## Parses and returns the [code]class_name[/code] from the provided [code]script[/code]'s source code
 static func get_class_name(script: GDScript) -> StringName:
 	if script:
 		var regex := RegEx.create_from_string("\\bclass_name\\b[\\s\\t\\n]*([^\\s\\t\\n]+)")
